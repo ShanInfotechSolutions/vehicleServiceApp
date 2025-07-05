0d40774e0d515e39f74cf75f57c615f835fb5511
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-07-05 16:32:27 +0530

    Main Class Modified

--- src/main/java/com/shanInfotech/vehicleServiceApp/VSMainApp.java
+++ src/main/java/com/shanInfotech/vehicleServiceApp/VSMainApp.java
@@ -2,11 +2,11 @@ package com.shanInfotech.vehicleServiceApp;
 
 import com.shanInfotech.vehicleServiceApp.daoImpl.VehicleDaoImpl;
 import com.shanInfotech.vehicleServiceApp.daoInterface.VehicleDao;
-import com.shanInfotech.vehicleServiceApp.entity.Vehicle;
+//import com.shanInfotech.vehicleServiceApp.entity.Vehicle;
 
 public class VSMainApp {
 	public static void main(String[] args) throws Exception {
-	    Vehicle v = new Vehicle(103, "Shanmukh", "Swift", "KA01AB9999");
+	    //Vehicle v = new Vehicle(106, "Shanmukh", "Swift", "KA01AB9999");
 	    VehicleDao dao = new VehicleDaoImpl();
 	   // dao.addVehicle();
 	    //System.out.println("Inserted: " + v);
